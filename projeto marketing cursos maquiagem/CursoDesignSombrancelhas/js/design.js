04d9687e77b6586821440e3037e46d057aa67b37
Feitass <lucas-feitosa16@live.com>
2022-04-05 19:17:29 -0300

    Modificação 05/04/2022
    Foi acrescentado mais 2 sistemas de slides para a página
    
    Foram acrescentadas mais algumas fotos para os slides
    
    Foi iniciado e concluído o footor

--- projeto marketing cursos maquiagem/CursoDesignSombrancelhas/js/design.js
+++ projeto marketing cursos maquiagem/CursoDesignSombrancelhas/js/design.js
@@ -3,11 +3,14 @@
 'use strict';
 
 const images = [
-    {'id': '1', 'url': 'imagens/maquiagem1jpeg.jpeg'},
-    {'id': '2', 'url': 'imagens/maquiagem2jpeg.jpeg'},
-    {'id': '3', 'url': 'imagens/maquiagem3jpeg.jpeg'},
-    {'id': '4', 'url': 'imagens/sombra1jpeg.jpeg'},
-    {'id': '5', 'url': 'imagens/sombra2jpeg.jpeg'},
+    {'id': '1', 'url': 'imagens/maquiagem1.jpeg'},
+    {'id': '2', 'url': 'imagens/maquiagem2.jpeg'},
+    {'id': '3', 'url': 'imagens/maquiagem3.jpeg'},
+    {'id': '4', 'url': 'imagens/maquiagem4.jpeg'},
+    {'id': '5', 'url': 'imagens/maquiagem5.jpeg'},
+    {'id': '6', 'url': 'imagens/maquiagem6.jpeg'},
+    {'id': '7', 'url': 'imagens/maquiagem7.jpeg'},
+    {'id': '8', 'url': 'imagens/maquiagem8.jpeg'},
 ]
 
 const containerItems = document.querySelector("#container-items")
@@ -37,7 +40,81 @@ document.querySelector('#previous').addEventListener('click',previous);
 document.querySelector('#next').addEventListener('click', next);
 
 
-/*SISTEMA DE BORDA ANIMADA*/
+/*******************************************************************************************/
 
+
+
+const images_2 = [
+    {'id': '1', 'url': 'imagens/epilacao1.jpeg'},
+    {'id': '2', 'url': 'imagens/epilacao2.jpeg'},
+    {'id': '3', 'url': 'imagens/epilacao3.jpeg'},
+   
+]
+
+const containerItems_2 = document.querySelector("#container-items2")
+
+const loadImages_2 = (images_2, container_2) => {
+images_2.forEach ( image_2 => {
+    container_2.innerHTML += `<div class="item-2">
+        <img src='${image_2.url}'>
+        </div>`
+})
+}
+loadImages_2(images_2, containerItems_2)
+let items_2 = document.querySelectorAll(".item-2")
+
+const previous2 = () => {
+    containerItems_2.appendChild(items_2[0]);
+    items_2 = document.querySelectorAll(".item-2");
+}
+
+const next2 = () => {
+    const lastItem_2 = items_2[items_2.length - 1];
+    containerItems_2.insertBefore(lastItem_2, items_2[0]);
+    items_2 = document.querySelectorAll('.item-2');
+}
+
+document.querySelector('#previous2').addEventListener('click',previous2);
+document.querySelector('#next2').addEventListener('click', next2);
+
+
+/*******************************************************************************************/
+
+
+
+const images_3 = [
+    {'id': '1', 'url': 'imagens/sombra1.jpeg'},
+    {'id': '2', 'url': 'imagens/sombra2.jpeg'},
+    {'id': '3', 'url': 'imagens/sombra3.jpeg'},
+    {'id': '4', 'url': 'imagens/sombra4.jpeg'},
+    {'id': '5', 'url': 'imagens/sombra5.jpeg'},
+    {'id': '6', 'url': 'imagens/sombra6.jpeg'},
+]
+
+const containerItems_3 = document.querySelector("#container-items3")
+
+const loadImages_3 = (images_3, container_3) => {
+images_3.forEach ( image_3 => {
+    container_3.innerHTML += `<div class="item-3">
+        <img src='${image_3.url}'>
+        </div>`
+})
+}
+loadImages_3(images_3, containerItems_3)
+let items_3 = document.querySelectorAll(".item-3")
+
+const previous3 = () => {
+    containerItems_3.appendChild(items_3[0]);
+    items_3 = document.querySelectorAll(".item-3");
+}
+
+const next3 = () => {
+    const lastItem_3 = items_3[items_3.length - 1];
+    containerItems_3.insertBefore(lastItem_3, items_3[0]);
+    items_3 = document.querySelectorAll('.item-3');
+}
+
+document.querySelector('#previous3').addEventListener('click',previous3);
+document.querySelector('#next3').addEventListener('click', next3);
 
 
